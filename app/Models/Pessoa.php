eea1ccac6b5e0b712a6d81801dab7b83676cadb4
Markame <mr.markame@gmail.com>
2024-04-11 20:22:03 -0300

    initial

---
+++ app/Models/Pessoa.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pessoa extends Model
+{
+    protected $fillable = ['nome', 'nome_social','endereco','numero_casa','bairro','ponto_ref','data_nascimento','estado_civil','cpf','rg','sexo','ocupacao','renda_mes','escolaridade',
+    'identidade_genero','nome_mae','nome_pai','religiao','etinia','telefone'];
+    use HasFactory;
+}
