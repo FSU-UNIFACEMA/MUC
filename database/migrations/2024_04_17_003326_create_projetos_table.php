2a212dfda25ebdf91d41c4353fbaee3a230dbadf
Markame <mr.markame@gmail.com>
2024-04-18 18:54:57 -0300

    /Modificaçoes em 18042024

---
+++ database/migrations/2024_04_17_003326_create_projetos_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projetos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome_projeto');
+            $table->text('descricao_projeto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projetos');
+    }
+};
